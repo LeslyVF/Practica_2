87af18e1ac08883a521758203100d18d6c5e3f1c
armentaspcs <armentaspcs@DESKTOP-AHUAJBN>
2022-09-14 09:15:26 -0700

    Agregar archivos de proyecto.

---
+++ Practicas/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practicas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Edad = 21;
+            string Nombre = "Lesly";
+            string Pais = "Alemania";
+
+            Console.WriteLine("Mi nombre es: {1}, tengo {0} de edad y me gustaria conocer el pais de {2}", Edad, Nombre, Pais);
+            Console.ReadKey(); 
+        }
+    }
+ 
+}
+
